389af96fd8e81f1c53ff08bbbab1e1ae59d86b9b
Adriabn D <adrian.danila22@gmail.com>
2023-05-09 17:49:45 +0300

    working on product-details-page

---
+++ src/app/components/breadcrumb-menu/breadcrumb-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-breadcrumb-menu',
+  templateUrl: './breadcrumb-menu.component.html',
+  styleUrls: ['./breadcrumb-menu.component.scss']
+})
+export class BreadcrumbMenuComponent {
+
+}
