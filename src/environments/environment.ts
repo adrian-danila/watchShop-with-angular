c9e85dd63a02f0e71a96adacfeefaee3e449c20e
Adriabn D <adrian.danila22@gmail.com>
2023-06-12 13:33:25 +0300

    changed background color on products tab

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+firebase : {
+  apiKey: "AIzaSyBryqoPqLRfsIv0wD2A2b06QTC2HWX0PjQ",
+  authDomain: "watchshop-angular-firebase.firebaseapp.com",
+  databaseURL: "https://watchshop-angular-firebase-default-rtdb.firebaseio.com",
+  projectId: "watchshop-angular-firebase",
+  storageBucket: "watchshop-angular-firebase.appspot.com",
+  messagingSenderId: "646302307504",
+  appId: "1:646302307504:web:7389c981fff465521bd775",
+  measurementId: "G-9TGW7VTR0W"
+  }
+};
