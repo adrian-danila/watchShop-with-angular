58c65c2810b98f13518d1fbfbeeb487d3380b7b0
Adriabn D <adrian.danila22@gmail.com>
2023-06-30 20:54:52 +0300

    added functionality view more for card-product in section two from dashboard

--- src/app/components/card-product/card-product.component.ts
+++ src/app/components/card-product/card-product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-product',
@@ -10,5 +10,4 @@ export class CardProductComponent {
 
   productData: any = [];
 
-  constructor() {}
 }
