040f469bbcef752d0b60745dd1e1f185df145082
Adriabn D <adrian.danila22@gmail.com>
2023-06-30 20:13:00 +0300

    added posibilites to switch photos in product details

--- src/app/pages/product-details/product-details.component.ts
+++ src/app/pages/product-details/product-details.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Data } from '@angular/router';
 import { DataService } from 'src/app/services/data/data.service';
 
-
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
@@ -12,6 +11,7 @@ export class ProductDetailsComponent implements OnInit {
   productId: string;
   product: any = {};
   fetchedData: any;
+  selectedImage: string;
 
   constructor(
     private route: ActivatedRoute,
@@ -24,15 +24,24 @@ export class ProductDetailsComponent implements OnInit {
       this.getProductDetails();
     });
 
-    this.dataService.fetchData().subscribe((data) =>{
+    this.dataService.fetchData().subscribe((data) => {
       this.fetchedData = data;
-    })
+    });
+
+    this.selectedImage = this.product.photos[0];
+  }
+
+  selectImage(photo: string) {
+    this.selectedImage = photo;
   }
 
   getProductDetails() {
     const adjutedProductId = parseInt(this.productId) - 1;
-    this.dataService.getProductById(adjutedProductId.toString()).subscribe((product) => {
-      this.product = product;
-    });
+    this.dataService
+      .getProductById(adjutedProductId.toString())
+      .subscribe((product) => {
+        this.product = product;
+        this.selectedImage = this.product.photos[0];
+      });
   }
 }
